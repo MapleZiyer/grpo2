e5470a6b38c08d4ae192982c5396b68f1a1547c7
MapleZiyer <904532745@qq.com>
2025-02-26 19:00:49 +0800

    grpo

--- run_train.sh
+++ run_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 设置CUDA可见设备
-export CUDA_VISIBLE_DEVICES=0,1,2,3  # 根据实际可用的GPU数量调整
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  # 根据实际可用的GPU数量调整
 
 # 使用torchrun启动分布式训练
 torchrun \
-    --nproc_per_node=4 \
+    --nproc_per_node=8 \
     --master_port=29500 \
     train.py
